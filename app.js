04cf5af5cb5a19579d217b9cb39b8eb299b532c7
MokeDuck <40128859+MokeDuck@users.noreply.github.com>
2020-02-20 13:57:10 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+  var config = {
+    apiKey: "AIzaSyCjakwu2GekBJq-CIuNwSvsRuWdsis6ocg",
+    authDomain: "duckfathers-quacks.firebaseapp.com",
+    databaseURL: "https://duckfathers-quacks.firebaseio.com",
+    projectId: "duckfathers-quacks",
+    storageBucket: "duckfathers-quacks.appspot.com",
+    messagingSenderId: "640595631598",
+    appId: "1:640595631598:web:626c5e98ee2ea5210542ff",
+    measurementId: "G-9518WDLB9K"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
