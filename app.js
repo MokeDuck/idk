01b600da73cd317565d406cccad4b720eb126364
MokeDuck <40128859+MokeDuck@users.noreply.github.com>
2020-02-23 20:42:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,19 @@
   };
   // Initialize Firebase
   firebase.initializeApp(config);
+  var firestore = firebase.firestore();
+
+const docRef = firestore.doc("samples/duckQuackData";)
+// idk if that semicolon is right, directly above. try putting it after the parentheses.
+const outputHeader = document.querySelector("#duckQuackOutput");
+const inputTestfield = document.querySelector("#latestDuckQuack");
+const saveButton = document.QuerySelector("#saveButton");
+
+saveButton.addEventListener("click", function() {
+  const textToSave = inputTextField.value;
+  console.log("I am going to save " + textToSave + " to Firestore);
+  docRef.set({
+    duckQuackStatus: textToSave
+              }).then(function() {
+    console.log("St
+})
